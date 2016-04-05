f1cdb3378ab6e11aa1868743ca190396c7a7f3d3
Vinicius <viniciussilva@olaria.me>
2016-04-05 15:40:37 -0300

    Ok

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 function stickyTitles(stickies) {
 
-    this.load = function() {
+	this.load = function() {
 
-			  stickies.each(function(){
-				
-				    var thisSticky = jQuery(this).wrap('<div class="followWrap" />');
-				        thisSticky.parent().height(thisSticky.outerHeight());
-				
-				    jQuery.data(thisSticky[0], 'pos', thisSticky.offset().top);
-
-			  });
-		}
-			
-		this.scroll = function() {
+		  stickies.each(function(){
 		
-			  stickies.each(function(i){				
-				
-				    var thisSticky = jQuery(this),
-					      nextSticky = stickies.eq(i+1),
-					      prevSticky = stickies.eq(i-1),
-					      pos = jQuery.data(thisSticky[0], 'pos');
-			
-				    if (pos <= (jQuery(window).scrollTop() + 0 )) {
-					
-					      thisSticky.addClass("fixed");
-					
-					      if (nextSticky.length > 0 && thisSticky.offset().top >= jQuery.data(nextSticky[0], 'pos') - thisSticky.outerHeight() - 0) {
-						
-						        thisSticky.addClass("absolute").css("top", jQuery.data(nextSticky[0], 'pos') - thisSticky.outerHeight());
+			    var thisSticky = jQuery(this).wrap('<div class="followWrap" />');
+				thisSticky.parent().height(thisSticky.outerHeight());
+		
+			    jQuery.data(thisSticky[0], 'pos', thisSticky.offset().top);
 
-					      }
-					
-				    } else {
-					
-					      thisSticky.removeClass("fixed");
-					
-					      if (prevSticky.length > 0 && jQuery(window).scrollTop() <= jQuery.data(thisSticky[0], 'pos')  - prevSticky.outerHeight() - 0) {
-					
-						        prevSticky.removeClass("absolute").removeAttr("style");
-					
-					      }
-					
-				    }
-		    });			
+		  });
+	}
+			
+	this.scroll = function() {
+
+		  stickies.each(function(i){				
+		
+			    var thisSticky = jQuery(this),
+				      nextSticky = stickies.eq(i+1),
+				      prevSticky = stickies.eq(i-1),
+				      pos = jQuery.data(thisSticky[0], 'pos');
+	
+			    if (pos <= (jQuery(window).scrollTop() + 0 )) {
+			
+				      thisSticky.addClass("fixed");
+			
+				      if (nextSticky.length > 0 && thisSticky.offset().top >= jQuery.data(nextSticky[0], 'pos') - thisSticky.outerHeight() - 0) {
+				
+						thisSticky.addClass("absolute").css("top", jQuery.data(nextSticky[0], 'pos') - thisSticky.outerHeight());
+
+				      }
+			
+			    } else {
+			
+				      thisSticky.removeClass("fixed");
+			
+				      if (prevSticky.length > 0 && jQuery(window).scrollTop() <= jQuery.data(thisSticky[0], 'pos')  - prevSticky.outerHeight() - 0) {
+			
+						prevSticky.removeClass("absolute").removeAttr("style");
+			
+				      }
+			
+			    }
+	    });			
     }
 }
 	
@@ -50,11 +50,16 @@ jQuery(document).ready(function(){
 	
 		var newStickies = new stickyTitles(jQuery(".followMeBar"));
 		
-		newStickies.load();
+		
 			
 		jQuery(window).on("scroll", function() {
 
 			  newStickies.scroll();
+		
+		});
+		jQuery(window).on("resize", function() {
+			newStickies.load();
+	
 		
 		});
 });
